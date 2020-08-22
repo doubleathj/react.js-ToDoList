0f9c74ec58c7205ac75ee30e55cf3b3600bea1ab
jun <doubleat.hj@gmail.com>
2020-08-22 22:14:45 +0900

    Bare Minimum Requirements 1
    component 만들고 rendering 방식 연습

--- src/index.js
+++ src/index.js
@@ -1,2 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
+const HelloWorld = () => <div>HELLOWORLD!!</div>;
+
+const OtherWorld = () => <div>OTHERWORLD!!</div>;
+
+const AnotherWorld = () => <div>ANOTEHRWORLD!!</div>;
+
+const App = () => (
+  <div>
+    슈퍼월드!
+    <HelloWorld />
+    <OtherWorld />
+    <AnotherWorld />
+  </div>
+);
+
+ReactDOM.render(<App />, document.getElementById('root'));
